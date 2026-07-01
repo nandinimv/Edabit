96e941907cd1153aa0509e9aaac26ff916bc0b00
nandinimv <nandinimv851@gmail.com>
2026-07-01 16:05:46 +0530

    new function

---
+++ area_of_triangle.py
@@ -0,0 +1,5 @@
+def area(base,height):
+    a= 0.5*base*height
+    return a
+value=area(3,2)
+print(value)
--- return_next_num.py
+++ return_next_num.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 def number(a):
-    return a+1
-result=number(9)
-=======
-def number(a):
-    return a+1
-result=number(9)
->>>>>>> 682113cfcfab4c5ee08b423b5ac19d62a5b67183
+    return a + 1
+
+result = number(9)
 print(result)
